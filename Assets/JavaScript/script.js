e9f1cf1f8ced9f25062d9d8d81a33638be47ddcb
MissingNewt <chelby.sallady@gmail.com>
2021-10-13 15:58:26 -0700

    uploading html and css

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,29 @@
+var cardContainer = docucment.getElementById('card-container');
+
+function appendEmployee(name, id, email){
+
+}
+
+function Employee(name, id, email){
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.getName = ()=>{
+
+    }
+    this.getEmail = ()=>{
+
+    }
+    this.getRole = ()=>{
+        
+    }
+    if(role === "engineer"){
+
+    }
+    if(role === "intern"){
+
+    }
+    if(role === "manager"){
+
+    }
+}
